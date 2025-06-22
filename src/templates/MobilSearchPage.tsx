f652c51fa946d5b5282a5522005cb750b9c8685b
NPOGULA <teamx.pogula@mbusa.com>
2025-06-22 16:55:28 +0530

    live issue fix

--- src/templates/MobilSearchPage.tsx
+++ src/templates/MobilSearchPage.tsx
@@ -1,6 +1,5 @@
 // pages/search.tsx
 import * as React from "react";
-
 import {
   Template,
   GetPath,
@@ -13,29 +12,29 @@ import {
   SearchHeadlessProvider,
   SearchConfig,
   provideHeadless,
-  useSearchActions,
-  VerticalResults
 } from "@yext/search-headless-react";
 
-// import SearchHeader from "../components/MobilSearch/SearchHeader";
+// Component imports
 import StationSearchBar from "../components/MobilSearch/StationSearchBar";
 import StationList from "../components/MobilSearch/StationList";
 import StationMap from "../components/MobilSearch/StationMap";
 
 import "../index.css";
 
+// -------------------- Config --------------------
+
 export const config: TemplateConfig = {
   stream: {
     $id: "mobil-search-page-stream",
     fields: ["id", "uid", "meta", "name", "slug"],
     filter: {
-      entityIds: ["mobil-search"]  // <-- must match entity ID created above
+      entityIds: ["mobil-search"], // <- matches your Knowledge Graph entity ID
     },
     localization: {
       locales: ["en"],
-      primary: true
-    }
-  }
+      primary: true,
+    },
+  },
 };
 
 export const getPath: GetPath<TemplateProps> = ({ document }) => {
@@ -43,34 +42,50 @@ export const getPath: GetPath<TemplateProps> = ({ document }) => {
 };
 
 export const getHeadConfig: GetHeadConfig<TemplateProps> = ({ document }) => ({
-  title: document.name
+  title: document.name,
 });
 
+// -------------------- Search Config --------------------
 
 const searchConfig: SearchConfig = {
   apiKey: "93809071953b06ec5b349f7ab68118e9",
   experienceKey: "locator-1",
   locale: "en",
-  verticalKey: "locations",       // <- use the one from your verticals
-  experienceVersion: "PRODUCTION",   // <- or "PRODUCTION"
-  headlessId: "locator-search",   // <- can be any string
+  verticalKey: "locations",
+  experienceVersion: "PRODUCTION",
+  headlessId: "locator-search",
 };
 
 const searcher = provideHeadless(searchConfig);
 
-const MobilSearchPage = () => {
+// -------------------- Page Template --------------------
+
+const MobilSearchPage: Template<TemplateProps> = () => {
   return (
     <SearchHeadlessProvider searcher={searcher}>
       <div className="flex flex-col h-screen">
         <div className="px-8 pt-6">
-          <h1 className="text-2xl font-semibold text-gray-800">Find a gas station near me</h1>
+          <h1 className="text-2xl font-semibold text-gray-800">
+            Find a gas station near me
+          </h1>
           <p className="text-gray-600 text-sm mt-2">
-            Type a town, postcode, or address and click the "Search Location" button.
-            You'll see a map and a listing of Exxon and Mobil service stations in the surrounding area.
-            <br />Click on "Get Directions" to get driving directions to the station, or "Station Details" to view more info.
+            Type a town, postcode, or address and click the "Search Location" button. <br />
+            You'll see a map and a listing of Exxon and Mobil service stations in the surrounding area. <br />
+            Click on "Get Directions" to get driving directions to the station, or "Station Details" to view more info.
           </p>
-          <a href="#" className="text-blue-700 underline text-sm mt-1 inline-block">See all Gas Station</a>
+          <a
+            href="#"
+            className="text-blue-700 underline text-sm mt-1 inline-block"
+          >
+            See all Gas Station
+          </a>
         </div>
+
+        {/* Optional: Add a search bar and results panel */}
+        {/* <StationSearchBar /> */}
+        {/* <StationList /> */}
+
+        {/* Map Component */}
         <StationMap />
       </div>
     </SearchHeadlessProvider>
