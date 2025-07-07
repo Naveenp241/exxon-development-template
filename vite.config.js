82ba3d3498fcf3b4a6946ba2ddf7de45b1c64fb0
NPOGULA <teamx.pogula@mbusa.com>
2025-07-07 15:10:09 +0530

    path fix

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import yextSSG from "@yext/pages/vite-plugin";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    yextSSG(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "src/assets/fonts/*",
-          dest: "assets/fonts",
-        },
-      ],
-    }),
-  ],
+  plugins: [react(), yextSSG()],
 });
